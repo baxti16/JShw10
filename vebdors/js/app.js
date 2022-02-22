a52b74e4237e27545d220d3edb313ff57fcb4c08
baxti16 <96297516+baxti16@users.noreply.github.com>
2022-02-22 18:15:37 +0500

    Add files via upload

---
+++ vebdors/js/app.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = 'white';
+var even = document.getElementsByClassName("card__item");
+even[0].style.backgroundColor = "aqua";
+even[2].style.backgroundColor = "aqua";
+even[4].style.backgroundColor = "aqua";
+even[6].style.backgroundColor = "aqua";
